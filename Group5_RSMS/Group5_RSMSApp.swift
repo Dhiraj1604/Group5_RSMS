98ae4b331b48632bb873b7a39da2b75d591a4144
Dhiraj1604 <dhirajbodake16@gmail.com>
2026-04-10 12:23:44 +0530

    Initial Commit

---
+++ Group5_RSMS/Group5_RSMSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Group5_RSMSApp.swift
+//  Group5_RSMS
+//
+//  Created by Dhiraj on 10/04/26.
+//
+
+import SwiftUI
+
+@main
+struct Group5_RSMSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
